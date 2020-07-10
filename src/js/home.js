993388f0de235f34fadcdf193cd1101711302792
Carlos Diaz <diazpolanco13@gmail.com>
2020-07-10 19:16:13 -0400

    primera fila de pelis, del api

--- src/js/home.js
+++ src/js/home.js
@@ -1,22 +1,54 @@
-
-const API =  'https://yts.mx/api/v2/list_movies.json';
+const API = "https://yts.mx/api/v2/list_movies.json";
 (async function load() {
+  console.log("Buscando peliculas...");
 
-    console.time()
-    console.log('Buscando peliculas...')
-
-    async function getData(genero) {
-    const result = await fetch(API+'?genre='+genero);
+  async function getData(genero) {
+    const result = await fetch(API + "?genre=" + genero);
     return await result.json();
-}
-    const actionList =  await getData('action');
-    const dramaList =  await getData('drama');
-    const animationList =  await getData('animation');
+  }
+  const actionList = await getData("action");
+  const dramaList = await getData("drama");
+  const animationList = await getData("animation");
 
-    console.log(actionList, dramaList, animationList);
+  console.log(actionList, dramaList, animationList);
 
-    console.timeEnd();
 
-    
+  function videoItemTemplate(movie) {
+    return `<div class="primaryPlaylistItem">
+        <div class="primaryPlaylistItem-image">
+        <img src="${movie.medium_cover_image}">
+        </div>
+        <h4 class="primaryPlaylistItem-title">
+        ${movie.title}
+        </h4>
+        </div>`;
+  }
 
-})()
+const $actionContainer = document.getElementById("action");
+  actionList.data.movies.forEach((movie) => {
+    const HTMLString = videoItemTemplate(movie);
+    const html = document.implementation.createHTMLDocument();
+    html.body.innerHTML = HTMLString;
+
+    $actionContainer.append(html.body.children[0])
+    console.log(HTMLString);
+  });
+
+
+  const $dramaContainer = document.getElementById("drama");
+  const $animationContainer = document.getElementById("animation");
+
+  const $modal = document.getElementById("modal");
+  const $overlay = document.getElementById("overlay");
+  const $hideModal = document.getElementById("hide-modal");
+
+  const $featuringContainer = document.getElementById("featuring");
+  const $form = document.getElementById("form");
+  const $home = document.getElementById("home");
+
+  const $modalImage = modal.querySelector("img");
+  const $modalTitle = modal.querySelector("h1");
+  const $modalDescription = modal.querySelector("p");
+
+
+})();
