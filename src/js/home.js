01ffc4579e626e5729fafc6d8bd1aaf321bd0fd6
Carlos Diaz <diazpolanco13@gmail.com>
2020-07-16 15:26:44 -0400

    guardando datos del cache

--- src/js/home.js
+++ src/js/home.js
@@ -102,17 +102,34 @@ const BASE_API = "https://yts.mx/api/v2/";
       addEventClick(movieElement);
     });
   }
+  async function cacheExist(category) {
+    const listName = `${category}List`
+    const cacheList = window.localStorage.getItem(listName);
+    if (cacheList) {
+      return JSON.parse(cacheList);
+    }
+    const { data: { movies: data } } = await getData(`${BASE_API}list_movies.json?genre=${category}`);
+    window.localStorage.setItem('listName', JSON.stringify(data));
+
+    return data;
+  }
 
 
-  const { data: { movies: actionList } } = await getData(`${BASE_API}list_movies.json?genre=action`);
+  // const { data: { movies: actionList} } = await getData(`${BASE_API}list_movies.json?genre=action`);
+  const actionList = await cacheExist('action');
+  window.localStorage.setItem('actionList', JSON.stringify(actionList));
   const $actionContainer = document.getElementById("action");
   renderMoviesList(actionList, $actionContainer, "action");
   
-  const { data: { movies: dramaList } } = await getData(`${BASE_API}list_movies.json?genre=drama`);
+  // const { data: { movies: dramaList } } = await getData(`${BASE_API}list_movies.json?genre=drama`);
+  const dramaList = await cacheExist('drama');
+  window.localStorage.setItem('dramaList', JSON.stringify(dramaList));
   const $dramaContainer = document.getElementById("drama");
   renderMoviesList(dramaList, $dramaContainer, "drama");
   
-  const { data: { movies: animationList } } = await getData(`${BASE_API}list_movies.json?genre=animation`);
+  // const { data: { movies: animationList } } = await getData(`${BASE_API}list_movies.json?genre=animation`);
+  const animationList = await cacheExist('animation');
+  window.localStorage.setItem('animationList', JSON.stringify(animationList));
   const $animationContainer = document.getElementById("animation");
   renderMoviesList(animationList, $animationContainer, "animation");
   
